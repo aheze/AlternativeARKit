a39800b4e3f34b520f14260ae8b9d90a540fd6fb
John <zjohnzheng@gmail.com>
2020-04-21 17:19:09 -0700

    Initial Commit

---
+++ AlternativeARKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlternativeARKit
+//
+//  Created by Zheng on 4/21/20.
+//  Copyright © 2020 Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
